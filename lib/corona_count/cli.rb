7f90c52013e1c42679d110a0bc3b917c90106629
Jack Quinoa <jquinoa208@gmail.com>
2020-06-28 02:49:39 +0000

    Failing but an idea

--- lib/corona_count/cli.rb
+++ lib/corona_count/cli.rb
@@ -3,15 +3,25 @@ require "httparty"
 
 class CoronaCount::CLI 
   
-# attr_accessor :state, :positive, :recovered, :death
+attr_accessor :state, :positive, :recovered, :death
   
-attributes = HTTParty.get("https://covidtracking.com/api/v1/states/current.json")
-binding.pry
+# attributes = HTTParty.get("https://covidtracking.com/api/v1/states/current.json")
+# binding.pry
   
-#   def initialize(attributes)
-#     attributes.each {|key, value| self.send(("#{key}="), value)}
-#     binding.pry 
-#   end
+  def initialize
+    attributes = HTTParty.get("https://covidtracking.com/api/v1/states/current.json")
+     binding.pry 
+    attributes.each {|key, value| self.send if 
+      key == state || key == positive || key == recovered || key == death then 
+      ("#{key}=", value)}
+    else nil 
+    end
+  end
+  
+def self.get_data 
+# c = Car.new()
+# params.each do |key, value|
+#   c.send("#{key}=", value)
   
   def call 
     puts "Hello there, please find your state below."  
