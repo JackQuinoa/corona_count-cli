0bcd9251d882d21961547cb946bd32d22418fa8a
Jack Quinoa <jquinoa208@gmail.com>
2020-06-28 23:45:32 +0000

    Initialize State

--- lib/corona_count/api_manager.rb
+++ lib/corona_count/api_manager.rb
@@ -1,11 +1,24 @@
 require "HTTParty"
 
 class CoronaCount::APIManager 
-
+  
 ATTRIBUTES = HTTParty.get("https://covidtracking.com/api/v1/states/current.json")
+  
+  def self.get_states
+    ATTRIBUTES.each {|info_hash|
+    state = info_hash["state"]
+    
+  end
+  
+  def self.get_states_data 
+  
+  end
+
+
 ATTRIBUTES.each {|info_hash| 
   state = info_hash["state"]
   positive = info_hash["positive"]
   recovered = info_hash["recovered"]
   death = info_hash["death"]
+  State.new(state, positive, recovered, death)
 end
--- lib/corona_count/state.rb
+++ lib/corona_count/state.rb
@@ -2,4 +2,7 @@ class CoronaCount::State
 
 attr_accessor :state, :positive, :recovered, :death
 
+  def initialize(state, positive, recovered, death)
+    @state, @positive, @recovered, @death = state, positive, recovered, death
+  end
 end
