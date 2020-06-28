e642ddd1fb014f90af4bde7d912700935f38a45c
Jack Quinoa <jquinoa208@gmail.com>
2020-06-28 23:55:27 +0000

    Updated Classes

--- lib/corona_count/api_manager.rb
+++ lib/corona_count/api_manager.rb
@@ -8,17 +8,18 @@ ATTRIBUTES = HTTParty.get("https://covidtracking.com/api/v1/states/current.json"
     ATTRIBUTES.each {|info_hash|
     state = info_hash["state"]
     
+    
   end
   
   def self.get_states_data 
-  
+    ATTRIBUTES.each {|info_hash| 
+    state = info_hash["state"]
+    positive = info_hash["positive"]
+    recovered = info_hash["recovered"]
+    death = info_hash["death"]
+    State.new(state, positive, recovered, death)
   end
 
 
-ATTRIBUTES.each {|info_hash| 
-  state = info_hash["state"]
-  positive = info_hash["positive"]
-  recovered = info_hash["recovered"]
-  death = info_hash["death"]
-  State.new(state, positive, recovered, death)
+ 
 end
--- lib/corona_count/state.rb
+++ lib/corona_count/state.rb
@@ -1,8 +1,21 @@
 class CoronaCount::State
 
 attr_accessor :state, :positive, :recovered, :death
+@@all = []
 
   def initialize(state, positive, recovered, death)
     @state, @positive, @recovered, @death = state, positive, recovered, death
+    @@all << self 
   end
+  
+  def state_list 
+    APIMananger.get_states
+    
+  end
+  
+  def state_count
+    APIMananger.get_states_data 
+  end
+  
+  
 end
